0320bb7d0f2b713b7fb532b5ded2b56ff8284273
Samuel-de-Oliveira <samwolfg13@gmail.com>
2025-12-29 20:54:50 -0300

    PulseIn

--- Courses/Arduino-Lumina/aula6/aula6.ino
+++ Courses/Arduino-Lumina/aula6/aula6.ino
@@ -2,12 +2,18 @@ byte buttom {8};
 byte led    {9};
 
 // TODO: Ver sobre pulseIn
+// TIP:  https://docs.arduino.cc/language-reference/en/functions/advanced-io/pulseIn/
 
 void setup() {
   pinMode(buttom, INPUT_PULLUP);
 }
 
 void loop() {
-  digitalWrite(led, !digitalRead(buttom));
-  delay(10);
+  unsigned int pressTime = pulseIn(
+      buttom, LOW
+  );
+
+  if (pressTime >= 500) {
+    digitalWrite(led, HIGH);
+  }
 }
